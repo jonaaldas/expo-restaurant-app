5f7fce1bac1d67c5cfef14e333d9c93aa3c5552c
Jonathan Aldas <jonaaldas@gmail.com>
2025-09-18 23:50:25 +0200

    hello world GO

--- go/main.go
+++ go/main.go
@@ -25,6 +25,10 @@ func main() {
 
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("pong")
 	})
