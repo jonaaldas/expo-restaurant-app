e964765d64d64b5c4e4fb5ec522d15680a6f7b48
Jonathan Aldas <jonaaldas@gmail.com>
2025-09-18 22:57:05 +0200

    clean up go

--- go/main.go
+++ go/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/joho/godotenv"
-	"github.com/jonaaldas/go-restaurant-crud/database"
 	"github.com/jonaaldas/go-restaurant-crud/handlers"
 )
 
@@ -22,32 +20,8 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	mongoClient, err := database.InitMongo()
-
-	if err != nil {
-		log.Fatalf("Mongo connection failed: %v", err)
-	}
-	log.Println("Mongo connected successfully")
-
-	dbName := "restaurant-app"
-	collectionName := "restaurants"
-	saved_restaurants := "saved_restaurants"
-	collection := mongoClient.Database(dbName).Collection(collectionName)
-	saved_restaurants_collection := mongoClient.Database(dbName).Collection(saved_restaurants)
-
-	// Create unique index on place_id field for saved restaurants
-	if err := database.CreateUniqueIndexOnPlaceID(context.Background(), saved_restaurants_collection); err != nil {
-		log.Printf("Warning: Could not create unique index on place_id: %v", err)
-	}
-
-	defer func() {
-		if derr := mongoClient.Disconnect(context.Background()); derr != nil {
-			log.Printf("Mongo disconnect error: %v", derr)
-		}
-	}()
-
 	// Initialize handlers
-	h := handlers.NewHandlers(collection, saved_restaurants_collection)
+	h := handlers.NewHandlers()
 
 	app := fiber.New()
 
@@ -57,23 +31,6 @@ func main() {
 
 	// search restaurants
 	app.Get("/api/search", h.SearchRestaurants)
-
-	// get a restaurant by id
-	app.Get("/api/restaurant/:placeId", h.GetRestaurant)
-
-	// save a restaurant by ID
-	app.Post("/api/save", h.SaveRestaurant)
-
-	// get all saved restaurants
-	app.Get("/api/restaurants", h.GetAllRestaurants)
-
-	// get all restaurants ids
-	app.Get("/api/restaurants/ids", h.GetAllRestaurantsIds)
-
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	app.Listen(":3000")
 }
